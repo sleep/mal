23d152c9675c7cfb70c381f85d5ce5e730e46f69
Sean Lee <freshdried@gmail.com>
2015-11-09 02:15:50 -0500

    started reader

--- reader.c
+++ reader.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pcre.h>
+#include <assert.h>
 #include "linkedlist.c"
 
 
@@ -68,5 +69,87 @@ LList* tokenizer(char* input) {
   pcre_free(re);
   return tokens;
 }
+
+
+typedef struct Reader Reader;
+struct Reader {
+  LList* tokens;
+  LNode* curr;
+  int pos;
+};
+
+void r_checkInvariants(Reader* r) {
+  //check existence
+  assert(r != NULL);
+  assert(r->tokens != NULL);
+  //check zero condition
+  if (r->tokens->length == 0) {
+    assert(r->curr == NULL);
+    assert(r->pos = -1);
+  }
+  if (r->tokens->length > 0){
+    assert(r->curr != NULL);
+    assert(r->pos >= 0);
+    assert(r->pos < r->tokens->length);
+
+    //check inclusion of curr element
+    /* int index = 0; */
+    LNode* curr = ll_get(r->tokens, 0);
+    for (int i = 0 ; i < r->tokens->length; i++) {
+      if (curr == r->curr) {
+        return;
+      }
+      curr = curr->next;
+    }
+    assert(0);
+  }
+
+}
+
+
+
+
+Reader* r_create(char* input) {
+  Reader* r = malloc(sizeof(Reader));
+  r->tokens = tokenizer(input);
+  assert(r->tokens != NULL);
+
+  if (r->tokens->length == 0) {
+    r->curr = NULL;
+    r->pos = -1;
+  } else {
+    r->curr = ll_get(r->tokens, 0);
+    r->pos = 0;
+  }
+  return r;
+}
+
+void r_free(Reader* r) {
+  ll_free(r->tokens);
+  free(r);
+}
+
+int r_hasNext(Reader* r) {
+  assert(r->tokens->length > 0);
+  return r->pos < r->tokens->length -1;
+}
+
+LNode* r_peek(Reader* r) {
+  return r->curr;
+}
+
+// Must check have next before we go next
+LNode* r_next(Reader* r) {
+  assert(r_hasNext(r));
+
+  LNode* output = r->curr;
+  r->curr = output->next;
+  r->pos += 1;
+  return output;
+  
+}
+
+
+
 
 
